24542db57f20276d02d20254a60227bc26b136ea
Alexandr <Alexandr.Florinskij@yandex.ua>
2019-07-18 17:27:12 +0300

    nit

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  Text,
+} from 'react-native';
+
+const Flor = () => {
+  return (
+    <Fragment>
+      <Text>Flor Module</Text>
+    </Fragment>
+  );
+};
+
+export default Flor;
